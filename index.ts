87bcb8696e0d54960c22540e880e8827dcb53ceb
Ming-der Wang <mingderwang@gmail.com>
2023-12-27 14:52:24 +0800

    ok for http upgrade to websocket, use wscat --connect http://localhost:3000

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 const server = Bun.serve({
-  port: 3000,
-  fetch(req) {
-    return new Response("Bun!");
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    return new Response("Upgrade failed :(", { status: 500 });
   },
+  websocket: {
+    message(ws, message) {
+      ws.send("echo back " + message);
+    }, // a message is received
+    open(ws) {}, // a socket is opened
+    close(ws, code, message) {}, // a socket is closed
+    drain(ws) {}, // the socket is ready to receive more data
+  }, 
 });
 
 console.log(`Listening on http://localhost:${server.port} ...`);
